9367b65f60ea6533482ef9a4d89c19a94d8902f0
Druthvikkratos <s.druthvik@gmail.com>
2026-08-24 14:39:31 +0530

    navigation shell + design polish + production build verification

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,16 +2,25 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-  path: '',
-  loadComponent: () => import('./features/dashboard/dashboard/dashboard').then((m) => m.Dashboard),
-},
-  {
-    path: 'templates',
-    loadComponent: () =>
-      import('./features/report-templates/template-list/template-list').then((m) => m.TemplateList),
-  },
-  {
-    path: 'runs',
-    loadComponent: () => import('./features/report-runs/run-list/run-list').then((m) => m.RunList),
+    path: '',
+    loadComponent: () => import('./layout/shell/shell').then((m) => m.Shell),
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./features/dashboard/dashboard/dashboard').then((m) => m.Dashboard),
+      },
+      {
+        path: 'templates',
+        loadComponent: () =>
+          import('./features/report-templates/template-list/template-list').then(
+            (m) => m.TemplateList,
+          ),
+      },
+      {
+        path: 'runs',
+        loadComponent: () =>
+          import('./features/report-runs/run-list/run-list').then((m) => m.RunList),
+      },
+    ],
   },
 ];
